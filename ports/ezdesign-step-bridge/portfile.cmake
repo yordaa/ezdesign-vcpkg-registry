7431b6e40057d403fb30dc8720de6bbdf0af36fd
Yang Song <yangsong1442@gmail.com>
2026-07-28 22:37:24 +0800

    feat(registry): publish source-built step bridge

---
+++ ports/ezdesign-step-bridge/portfile.cmake
@@ -0,0 +1,18 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO yordaa/ezdesign-step-bridge
+    REF "v${VERSION}"
+    SHA512 5b2faf2d2867bb9f406d80a31be9cd5b8457874d62887ae01a6696a1864e674d901360ec3ac4f2cf89236e26afca87af5ad575c3ad02741708ac3d3d93ae4ef3
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+vcpkg_cmake_install()
+vcpkg_copy_tools(TOOL_NAMES ezd2step AUTO_CLEAN)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
--- ports/opencascade/portfile.cmake
+++ ports/opencascade/portfile.cmake
@@ -1,3 +1,5 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
 string(REPLACE "." "_" VERSION_STR "V${VERSION}")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -7,37 +9,14 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         fix-install-prefix-path.patch
-        drop-bin-letter-d.patch
-        dependencies.patch
         install-include-dir.patch
-        remove-vcpkg-enabling.patch
 )
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(BUILD_TYPE "Shared")
-else()
-    set(BUILD_TYPE "Static")
-endif()
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        freeimage   USE_FREEIMAGE
-        freetype    USE_FREETYPE
-        rapidjson   USE_RAPIDJSON
-        samples     INSTALL_SAMPLES
-        tbb         USE_TBB
-        vtk         USE_VTK
-)
-
-# We turn off BUILD_MODULE_Draw as it requires TCL 8.6 and TK 8.6 specifically which conflicts with vcpkg only having TCL 9.0 
-# And pre-built ActiveTCL binaries are behind a marketing wall :(
-# We use the Unix install layout for Windows as it matches vcpkg
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        ${FEATURE_OPTIONS}
         -DBUILD_ADDITIONAL_TOOLKITS=TKDESTEP
-        -DBUILD_LIBRARY_TYPE=${BUILD_TYPE}
+        -DBUILD_LIBRARY_TYPE=Shared
         -DBUILD_MODULE_ApplicationFramework=OFF
         -DBUILD_MODULE_DataExchange=OFF
         -DBUILD_MODULE_Draw=OFF
@@ -47,20 +26,30 @@ vcpkg_cmake_configure(
         -DBUILD_MODULE_Visualization=OFF
         -DBUILD_DOC_Overview=OFF
         -DBUILD_MODULE_DETools=OFF
+        -DBUILD_USE_VCPKG=OFF
         -DINSTALL_DIR_LAYOUT=Unix
         -DINSTALL_DIR_DOC=share/trash
-        -DINSTALL_DIR_SCRIPT=share/trash # not relocatable
+        -DINSTALL_DIR_SCRIPT=share/trash
         -DINSTALL_TEST_CASES=OFF
+        -DUSE_D3D=OFF
+        -DUSE_DRACO=OFF
+        -DUSE_EIGEN=OFF
+        -DUSE_FFMPEG=OFF
+        -DUSE_FREEIMAGE=OFF
+        -DUSE_FREETYPE=OFF
+        -DUSE_GLES2=OFF
+        -DUSE_OPENGL=OFF
+        -DUSE_OPENVR=OFF
+        -DUSE_RAPIDJSON=OFF
+        -DUSE_TBB=OFF
         -DUSE_TK=OFF
-    OPTIONS_DEBUG
-        -DINSTALL_SAMPLES=OFF
+        -DUSE_VTK=OFF
+        -DUSE_XLIB=OFF
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/opencascade)
 
-#make occt includes relative to source_file
 file(GLOB extra_headers
     LIST_DIRECTORIES false
     RELATIVE "${CURRENT_PACKAGES_DIR}/include/opencascade"
@@ -71,10 +60,6 @@ file(GLOB files "${CURRENT_PACKAGES_DIR}/include/opencascade/*.[hgl]xx")
 foreach(file_name IN LISTS files)
     vcpkg_replace_string("${file_name}" "(# *include) <([a-zA-Z0-9_]*[.][hgl]xx|${extra_headers})>" [[\1 "\2"]] REGEX IGNORE_UNCHANGED)
 endforeach()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/opencascade/Standard_Macro.hxx" "defined(OCCT_STATIC_BUILD)" "(1)")
-endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
