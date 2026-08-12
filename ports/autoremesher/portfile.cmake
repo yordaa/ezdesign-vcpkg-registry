1c5eacc35c7217748951d75ea5d17759ff150129
Yang Song <yangsong1442@gmail.com>
2026-08-12 11:18:46 +0800

    fix: remove empty AutoRemesher debug package

--- ports/autoremesher/portfile.cmake
+++ ports/autoremesher/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_copy_tools(TOOL_NAMES autoremesher-cli AUTO_CLEAN)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 if(VCPKG_TARGET_IS_OSX)
     file(GLOB TBB_RUNTIME "${CURRENT_INSTALLED_DIR}/lib/libtbb.*dylib")
